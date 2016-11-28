8e8e593ff375b5b5a5636a91bf83c2c420962713
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2016-11-27 20:43:45 -0700

    New build scripts.

---
+++ compile.linux.release.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -O2 -I/usr/include/SDL2 *.cpp -lSDL2main -lSDL2 -lSDL2_image -lGL -lGLEW
---
+++ compile.pi.debug.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 `sdl2-config --cflags` *.cpp `sdl2-config --libs` -L/opt/vc/lib -lbcm_host -lGLESv2
